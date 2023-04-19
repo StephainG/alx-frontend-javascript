9a63ad03f184917a899555943b647d6e39ff6507
Kunle Oni <onikunle03@gmail.com>
2023-04-19 23:51:08 +0100

    JS_task

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((values) => {
+    ]).then((values) => {
     const arr = [];
     for (const item of values) {
       arr.push({ status: item.status, value: item.value || item.reason });
