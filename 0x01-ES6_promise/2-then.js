e07c58cd0d5ee74194209313536fb3d938286a13
Kunle Oni <onikunle03@gmail.com>
2023-04-19 23:12:00 +0100

    JS_task

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,6 @@
 export default function handleResponseFromAPI(promise) {
     return new Promise((resolve, reject) => {
-        const object = {
-            status: 200,
-            body: 'success',
-        }
-        promise.then(() => resolve(object))
-        .catch((error) => reject(error))
-        .finally(() => {console.log('Got a response from the API')});
+        const object = {status: 200,body: 'success',}
+        promise.then(() => resolve(object)).catch((error) => reject(error)).finally(() => {console.log('Got a response from the API')});
     });
 }
